3f3ddcd153bcaab1e10443d1c791cabe2dd9efc9
Tyler Agee <tyler@pyroturtle.com>
2012-09-13 17:59:59 -0700

    moving models around to the correct app

--- issues/models.py
+++ issues/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 
 # Create your models here.
-'''
-	this class is for the actual issues/series management side of things
-'''
-class Comic(models.Model):
-	name = models.CharField(max_length=255)
-	number = models.CharField(max_length=255)  ###ususally numbers, but there's a decent amount of A, B, C, etc
-	year = models.CharField(max_length=255,blank=True,null=True) ###should be a date or year stamp
-	series = models.ForeignKey(Series)
 
 '''
 	To really keep track of things, every book needs to be part of a series; 
@@ -21,6 +13,17 @@ class Series(models.Model):
 	series_max = models.CharField(max_length=255,blank=True,null=True) ## useful for old series and mini series, a la Flashpoint
 	publisher = models.CharField(max_length=255,blank=True,null=True) ### could turn in to a select given how few there really are
 	new52_flag = models.BooleanField(default=False)
+
+'''
+	this class is for the actual issues/series management side of things
+'''
+class Comic(models.Model):
+	name = models.CharField(max_length=255)
+	number = models.CharField(max_length=255)  ###ususally numbers, but there's a decent amount of A, B, C, etc
+	year = models.CharField(max_length=255,blank=True,null=True) ###should be a date or year stamp
+	series = models.ForeignKey(Series)
+
+
 	
 #### THESE SHOULD GO IN THERE OWN APPS
 
