03cfb6584379e19c672ace5762c69c941c9b04f1
Tyler Agee <tyler@pyroturtle.com>
2012-09-13 18:09:20 -0700

    getting admin more up to speed

---
+++ ratings/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from ratings.models import UserRating
+from issues.models import Series
+from issues.models import Comic
+from comicFiles.models import ComicFile
+from django.contrib.admin.templatetags.admin_list import date_hierarchy
+
+#class ComicFileAdmin(admin.ModelAdmin):
+
+admin.site.register(UserRating)
