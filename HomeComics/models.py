17990dc6e1a3f479cefe94a790800ef3120437b6
Tyler Agee <tyler@pyroturtle.com>
2012-09-17 08:28:40 -0700

    adding HomeComics

---
+++ HomeComics/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+from issues.models import Comic
+# Create your models here.
+class HomeComic(models.Model):
+    user = models.ForeignKey(User)
+    comic = models.ForeignKey(Comic)
+    cover = models.CharField(max_length=255,blank=True,null=True)
+    box = models.CharField(max_length=255,blank=True,null=True)
+    condition = models.CharField(max_length=255,blank=True,null=True)
+    def __unicode__(self):
+        return self.user.username + "'s " + self.comic.name
