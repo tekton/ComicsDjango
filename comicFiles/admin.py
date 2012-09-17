9f8266585386d23ba1999ed780cb3d02bba1e1c0
Tyler Agee <tyler@pyroturtle.com>
2012-09-16 22:30:12 -0700

    file and folder processing of basic data complete

--- comicFiles/admin.py
+++ comicFiles/admin.py
@@ -3,8 +3,24 @@ from ratings.models import UserRating
 from issues.models import Series
 from issues.models import Comic
 from comicFiles.models import ComicFile
+from comicFiles.models import RootFolder
+from comicFiles import file_parsing
 from django.contrib.admin.templatetags.admin_list import date_hierarchy
+from comicFiles.file_parsing import parse_folder
 
-#class ComicFileAdmin(admin.ModelAdmin):
-    
+def folder_parse(modeladmin, request, queryset):
+    #print modeladmin
+    #print request
+    print queryset
+    for q in queryset:
+        #print q
+        parse_folder(q)
+
+class RootFolderAdmin(admin.ModelAdmin):
+    list_display = ['uri', 'last_scanned']
+    ordering = ['uri','last_scanned']
+    actions = [folder_parse]
+
+folder_parse.short_description = "Parse Folder For New Comics"
 admin.site.register(ComicFile)
+admin.site.register(RootFolder,RootFolderAdmin)
--- comicFiles/models.py
+++ comicFiles/models.py
@@ -6,12 +6,15 @@ from issues.models import Comic
 class RootFolder(models.Model):
     uri = models.CharField(max_length=255)
     last_scanned = models.DateTimeField(blank=True,null=True)
+    def __unicode__(self):
+        return self.uri
     
 class ComicFile(models.Model):
-    ### The one things they'll all have
+    ### The one things they'll all have...when you combine them!
     name = models.CharField(max_length=255)
+    dir_path = models.CharField(max_length=255)
     ### Should have, but in case they're not needed for some reason...
-    folder = models.ForeignKey(RootFolder,blank=True,null=True)
+    rootFolder = models.ForeignKey(RootFolder,blank=True,null=True)
     extension = models.CharField(max_length=255,blank=True,null=True)
     ### Parsing potential...
     error_flag = models.BooleanField(default=False)
@@ -22,7 +25,8 @@ class ComicFile(models.Model):
     comic_issue = models.CharField(max_length=255,blank=True,null=True)
     comic_volume = models.CharField(max_length=255,blank=True,null=True)
     comic_year = models.CharField(max_length=255,blank=True,null=True)
-    comic_most_likely = models.ForeignKey(Comic,blank=True,null=True)
+    comic_date = models.CharField(max_length=255, blank=True, null=True)
+    ###comic_most_likely = models.ForeignKey(Comic,blank=True,null=True) ### TO DO: implement
     ### Once "confirmed" these values are possible
     covers = "" #files could have one or more covers in them! Array of keys?! 
     '''
@@ -30,3 +34,9 @@ class ComicFile(models.Model):
         OR just use a general "tag" style, with a parent|name|val setup? or just db.covers['parent','val']
     '''
     comic = models.ForeignKey(Comic,blank=True,null=True)
+    
+    class Meta:
+        unique_together = ('name','dir_path')
+    
+    def __unicode__(self):
+        return self.name + " (" + self.dir_path +")"
