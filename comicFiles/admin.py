b1e1ce3da81f0b230c0ce588348d7244a7ef48f6
Tyler Agee <tyler@pyroturtle.com>
2012-12-23 21:33:09 -0800

    updating thumbnail testing and processing

--- comicFiles/admin.py
+++ comicFiles/admin.py
@@ -13,42 +13,45 @@ from comicFiles.images import rar_parse
 from comicFiles.images import zip_parse
 
 def folder_parse(modeladmin, request, queryset):
-	#print modeladmin
-	#print request
-	print queryset
-	for q in queryset:
-		#print q
-		print q.id
-		parse_folder(q)
+    #print modeladmin
+    #print request
+    print queryset
+    for q in queryset:
+        #print q
+        print q.id
+        parse_folder(q)
 
 def reparse_comic(modeladmin, request, queryset):
-	#print modeladmin
-	#print request
-	print queryset
-	for q in queryset:
-		#print q
-		print q.id
-		re_parse_file(q)	
+    #print modeladmin
+    #print request
+    print queryset
+    for q in queryset:
+        #print q
+        print q.id
+        re_parse_file(q)    
 
 def reparse_image(modeladmin,request,queryset):
-	for q in queryset:
-		if q.extension == "cbr":
-			q.thumbnail = rar_parse(q.dir_path, q.name)
-		elif q.extension == "cbz":
-			q.thumbnail = zip_parse(q.dir_path, q.name)
-		q.save()
+    for q in queryset:
+        if q.thumbnail is None:
+            print "Thumbnail already exist...most likely."
+            pass
+        elif q.extension == "cbr":
+            q.thumbnail = rar_parse(q.dir_path, q.name)
+        elif q.extension == "cbz":
+            q.thumbnail = zip_parse(q.dir_path, q.name)
+        q.save()
 
 class RootFolderAdmin(admin.ModelAdmin):
-	list_display = ['uri', 'last_scanned']
-	ordering = ['uri','last_scanned']
-	actions = [folder_parse]
+    list_display = ['uri', 'last_scanned']
+    ordering = ['uri','last_scanned']
+    actions = [folder_parse]
 
 class ComicFileAdmin(admin.ModelAdmin):
-	list_display = ['id','name','dir_path','rootFolder']
-	ordering = ['id','name','dir_path','error_flag']
-	actions = [reparse_comic,reparse_image]
-	list_filter = ['rootFolder','error_flag','review_flag']
-	search_fields = ['name','dir_path']
+    list_display = ['id','name','dir_path','rootFolder']
+    ordering = ['id','name','dir_path','error_flag']
+    actions = [reparse_comic,reparse_image]
+    list_filter = ['rootFolder','error_flag','review_flag']
+    search_fields = ['name','dir_path']
 
 folder_parse.short_description = "Parse Folder For New Comics"
 reparse_comic.short_description = "ReParse the selected files"
--- comicFiles/images.py
+++ comicFiles/images.py
@@ -12,116 +12,116 @@ from django.conf import settings
 #img_root = settings.IMG_ROOT
 
 def rar_parse(dir_path,name):
-	img_root = settings.IMG_ROOT
-	r = rarfile
-	if r.is_rarfile(dir_path+"/"+name):
-		r = rarfile.RarFile(dir_path+"/"+name)
-		try:
-			r.extract(r.namelist()[0],img_root)
-		except:
-			print "No unrar image for you..."
-		f_name = str(r.namelist()[0]).replace("\\","/")
-		### todo: check for if the replace worked, or for \ ???
-		return thumbnail_create(f_name)
-	else:
-		print "Not actually a RAR :("
-		return False
-	
+    img_root = settings.IMG_ROOT
+    r = rarfile
+    if r.is_rarfile(dir_path+"/"+name):
+        r = rarfile.RarFile(dir_path+"/"+name)
+        try:
+            r.extract(r.namelist()[0],img_root)
+        except:
+            print "No unrar image for you..."
+        f_name = str(r.namelist()[0]).replace("\\","/")
+        ### todo: check for if the replace worked, or for \ ???
+        return thumbnail_create(f_name)
+    else:
+        print "Not actually a RAR :("
+        return False
+    
 def zip_parse(dir_path,name):
-	img_root = settings.IMG_ROOT
-	z = zipfile
-	if z.is_zipfile(dir_path+"/"+name):
-		z = zipfile.ZipFile(dir_path+"/"+name)
-		try:
-			z.extract(z.namelist()[0],img_root)
-		except:
-			print "No unzipped image for you!"
-		### don't need to edit the rar directory seperator
-		return thumbnail_create(z.namelist()[0])
-	else:
-		print "Not actually a zip :("
-		return False
-	
+    img_root = settings.IMG_ROOT
+    z = zipfile
+    if z.is_zipfile(dir_path+"/"+name):
+        z = zipfile.ZipFile(dir_path+"/"+name)
+        try:
+            z.extract(z.namelist()[0],img_root)
+        except:
+            print "No unzipped image for you!"
+        ### don't need to edit the rar directory seperator
+        return thumbnail_create(z.namelist()[0])
+    else:
+        print "Not actually a zip :("
+        return False
+    
 def thumbnail_create(f_name):
-	img_root = settings.IMG_ROOT
-	if os.path.isfile(img_root+"/"+f_name):
-		print "should resize it to a real thumbnail..."
-	
-		try:
-			img = Image.open(img_root+"/"+f_name)
-		except:
-			print "no img?!"
-			return False
-	
-		basewidth = 200
-		wpercent = (basewidth / float(img.size[0]))
-		hsize = int((float(img.size[1]) * float(wpercent)))
-		img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
-		img.save(img_root+"/"+f_name)
-		return f_name
-	else:
-		print "Can't find the image that should have been extracted for:: "+f_name
-		return False
+    img_root = settings.IMG_ROOT
+    if os.path.isfile(img_root+"/"+f_name):
+        print "should resize it to a real thumbnail..."
+    
+        try:
+            img = Image.open(img_root+"/"+f_name)
+        except:
+            print "no img?!"
+            return False
+    
+        basewidth = 200
+        wpercent = (basewidth / float(img.size[0]))
+        hsize = int((float(img.size[1]) * float(wpercent)))
+        img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+        img.save(img_root+"/"+f_name)
+        return f_name
+    else:
+        print "Can't find the image that should have been extracted for:: "+f_name
+        return False
 
 def walkit():
-	for dir_path,dir_names,files in os.walk(dir_root):
-		for name in files:
-			extension = os.path.splitext(name)[1][1:]
-		
-			if extension == "cbz":
-			    z = zipfile
-				print "process as zip!"
-				if z.is_zipfile(name):
-					z = zipfile.ZipFile(name)
-					#for a in z.namelist():
-						#print a
-					print z.namelist()[0]
-				
-					z.extract(z.namelist()[0],img_root)
-					print "zip: "+str(z.namelist()[0])
-					if os.path.isfile(img_root+z.namelist()[0]):
-						print "should resize it to a real thumbnail..."
-				else:
-					print "not a zip..."
-			elif extension == "cbr":
-			    r = rarfile
-				print "process as rar!"
-				if r.is_rarfile(name):
-					print "rar to process"
-					r = rarfile.RarFile(name)
-					print r.namelist()[0]
-					#print r.infolist()[0]
-					#for f in r.infolist():
-					#	print f.filename, f.file_size
-					#	r.extract(f)
-				
-					#### TODO: check to see if the file exists and if we need to make a thumbnail...
-				
-					try:
-						r.extract(r.namelist()[0],img_root)
-						print "rar: "+str(r.namelist()[0])
-					except:
-						print "No image for you..."
-					
-					f_name = str(r.namelist()[0]).replace("\\","/")
-					if os.path.isfile(img_root+f_name):
-						print "should resize it to a real thumbnail..."
-					
-						try:
-							img = Image.open(img_root+f_name)
-						except:
-							print "no img?!"
-					
-						basewidth = 160
-						wpercent = (basewidth / float(img.size[0]))
-						hsize = int((float(img.size[1]) * float(wpercent)))
-						img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
-						img.save(img_root+f_name+".jpg")
-					
+    for dir_path,dir_names,files in os.walk(dir_root):
+        for name in files:
+            extension = os.path.splitext(name)[1][1:]
+        
+            if extension == "cbz":
+                z = zipfile
+                print "process as zip!"
+                if z.is_zipfile(name):
+                    z = zipfile.ZipFile(name)
+                    #for a in z.namelist():
+                        #print a
+                    print z.namelist()[0]
+                
+                    z.extract(z.namelist()[0],img_root)
+                    print "zip: "+str(z.namelist()[0])
+                    if os.path.isfile(img_root+z.namelist()[0]):
+                        print "should resize it to a real thumbnail..."
+                else:
+                    print "not a zip..."
+            elif extension == "cbr":
+                r = rarfile
+                print "process as rar!"
+                if r.is_rarfile(name):
+                    print "rar to process"
+                    r = rarfile.RarFile(name)
+                    print r.namelist()[0]
+                    #print r.infolist()[0]
+                    #for f in r.infolist():
+                    #   print f.filename, f.file_size
+                    #   r.extract(f)
+                
+                    #### TODO: check to see if the file exists and if we need to make a thumbnail...
+                
+                    try:
+                        r.extract(r.namelist()[0],img_root)
+                        print "rar: "+str(r.namelist()[0])
+                    except:
+                        print "No image for you..."
+                    
+                    f_name = str(r.namelist()[0]).replace("\\","/")
+                    if os.path.isfile(img_root+f_name):
+                        print "should resize it to a real thumbnail..."
+                    
+                        try:
+                            img = Image.open(img_root+f_name)
+                        except:
+                            print "no img?!"
+                    
+                        basewidth = 160
+                        wpercent = (basewidth / float(img.size[0]))
+                        hsize = int((float(img.size[1]) * float(wpercent)))
+                        img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+                        img.save(img_root+f_name+".jpg")
+                    
 
-					'''
-					try:
-						r.extract(r.namelist()[0],"./images")
-					except:
-						print "extraction failed..."
-					'''
+                    '''
+                    try:
+                        r.extract(r.namelist()[0],"./images")
+                    except:
+                        print "extraction failed..."
+                    '''
