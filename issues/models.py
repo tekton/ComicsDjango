07eba99983f087b44052257450804216145ca195
Tyler Agee <tyler@pyroturtle.com>
2012-12-29 18:47:45 -0800

    merging models

--- issues/models.py
+++ issues/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-'''
+
+class Series(models.Model):
+	'''
 	To really keep track of things, every book needs to be part of a series; 
 	the hard part is going to be graphic novels but those should be their own model type with a list of series/issues
-'''
-class Series(models.Model):
+	'''
 	name = models.CharField(max_length=255)
 	volume = models.CharField(max_length=255,blank=True,null=True)
 	mini_series_flag = models.BooleanField(default=False)
@@ -16,12 +17,13 @@ class Series(models.Model):
 	marvel_now = models.BooleanField(default=False)
 	def __unicode__(self):
 		return self.name + " v" + self.volume
-'''
-	this class is for the actual issues/series management side of things
-'''
+
 class Comic(models.Model):
+	'''
+		this class is for the actual issues/series management side of things
+	'''
 	name = models.CharField(max_length=255)
-	number = models.CharField(max_length=255)  ###ususally numbers, but there's a decent amount of A, B, C, etc
+	number = models.CharField(max_length=255)  ###ususally numbers, but there's a decent amount of A, B, C, 50.1, etc
 	year = models.CharField(max_length=255,blank=True,null=True) ###should be a date or year stamp
 	series = models.ForeignKey(Series)
 	read = models.BooleanField(default=False)
