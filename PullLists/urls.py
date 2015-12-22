5bbe3533116ceff757605ce4a222533f9c4b59a3
Tyler Agee <tyler@pyroturtle.com>
2015-12-21 23:05:45 -0800

    testing new api versioning method

--- PullLists/urls.py
+++ PullLists/urls.py
@@ -1,12 +1,21 @@
 from django.conf.urls import *
 from PullLists import views
 
+api_1_0 = [
+    url(r"^list", views.api_current_list, name="list")
+]
+
+apis = [
+    url(r"^1.0/", include(api_1_0, namespace="1.0"))
+]
+
 urlpatterns = [
     url(r'add/(\d+)', views.addToPullList, name="add"),
     url(r'covers', views.recentPullListCovers, name="covers"),
     url(r'delete/(\d+)', views.deleteList, name="delete"),
     url(r'owned', views.ownedSeriesList, name="owned"),
     url(r'missing', views.missing, name="missing"),
-    url(r"api/1.0/list", views.api_current_list, name="api_current"),
+    # url(r"api/1.0/list", views.api_current_list, name="api_current"),
+    url(r"api/", include(apis, namespace="api")),
     url(r'^', views.currentList, name="index"),
 ]
